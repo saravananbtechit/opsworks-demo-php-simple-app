9a298efcde8fb0c8056716c34d1ee78afa0c97ad
saravananbtechit <saravananbtechit@hotmail.com>
2015-02-18 15:34:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,3 +27,17 @@
     </body>
 
 </html>
+<?php
+$servername = "opsworkstestrdsinstance.cj5abbh2tkav.us-west-2.rds.amazonaws.com";
+$username = "itcopsworks";
+$password = "itcopsworks";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
